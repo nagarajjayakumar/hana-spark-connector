2a6b36533d7c21fad6686c442b2b20c74fe9d8d6
nagarajjayakumar <nagaraj.jayakumar@gmail.com>
2018-04-16 16:56:20 -0400

    faas

---
+++ src/main/scala/com/hortonworks/faas/spark/connector/util/Loan.scala
@@ -0,0 +1,15 @@
+package com.hortonworks.faas.spark.connector.util
+
+class Loan[A <: AutoCloseable](resource: A) {
+  def to[T](handle: A => T): T = {
+    try {
+      handle(resource)
+    } finally {
+      resource.close
+    }
+  }
+}
+
+object Loan {
+  def apply[A <: AutoCloseable](resource: A): Loan[A] = new Loan(resource)
+}
